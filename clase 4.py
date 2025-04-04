09fcdd5afe2bb1fa83e96f537a94285a1c9430a6
Axel Diaz <diazcabreraalanaxel44@gmail.com>
2025-04-04 15:30:36 +0000

    Cambie

---
+++ clase 4.py
@@ -0,0 +1,27 @@
+# Tipo de dato entero (int)
+numero_entero = 42
+numeroenter = 42
+
+# Tipo de dato flotante (float)
+numero_flotante = 3.14
+
+# Tipo de dato cadena de texto (str)
+cadena = "Hola, Mundo"
+
+# Tipo de dato booleano (bool)
+es_verdadero = True
+es_falso= False
+
+# tipo de dato lista (list)
+lista = []
+lista = [1, 2, 3, "cuatro" , 5.6]
+
+# Tipo de conjunto (set)
+conjunto = {1, 2, 3, 5, 5}
+
+# Tipo de diccionario (dict)
+diccionario = {"nombre": "Juan" , "edad": 30}
+alumno = {"nombre": "Cesar", "edad": 20}
+
+# Tipo de dato None(NoneType)
+nada = None 0
