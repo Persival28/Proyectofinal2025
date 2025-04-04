1c8424944d495b610f208f9f6c9f531f0ec93f01
CESARPAKUNA <gerardocasta118@gmail.com>
2025-04-04 15:37:19 +0000

    personajes

---
+++ personajes.py
@@ -0,0 +1,15 @@
+# Listas de personajes de un videojuego
+
+# Lista de personajes principales
+personajes_principales = ["Mario", "Luigi", "Peach", "Yoshi"]
+
+# Lista de villanos
+villanos = ["Bowser", "Wario", "Waluigi", "Kamek"]
+
+# Lista de personajes secundarios
+personajes_secundarios = ["Toad", "Daisy", "Rosalina", "Donkey Kong"]
+
+# Imprimir las listas
+print("Personajes principales:", personajes_principales)
+print("Villanos:", villanos)
+print("Personajes secundarios:", personajes_secundarios)
